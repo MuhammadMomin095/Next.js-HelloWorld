d6cf7769f017d1efad1b74c48b48f22625e882fe
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-10-17 01:36:39 +0500

    Next.js Hello_World Completed

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500">
+           hello-world created by momin
+      </h1>
+      <h6>congratulations </h6>
+    </div>
+  );
+}
